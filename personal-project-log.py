c45defc03bb18c70e0d525f1b7cac05f81485310
Robert Grüning <r.gruening@web.de>
2025-12-02 23:27:32 +0100

    inlined methods

--- personal-project-log.py
+++ personal-project-log.py
@@ -8,21 +8,14 @@ from repository import Repository
 #class ProjectEncoder(JSONEncoder):
 #    def default(self, o):
 #        return o.__dict__
-        
-def convert_to_projects(json_projects):
-    return ProjectConverter()\
-        .convert(json_projects)
 
-def load_data():
-    return convert_to_projects(
-        Repository()\
-            .set_file_path("projects.json")\
-            .load()
-    )
+json_projects = Repository()\
+    .set_file_path("projects.json")\
+    .load()
 
-def export_format_a(projects:list):
-    FormatAExporter()\
-        .set_file_path(f"{datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S_Projekte.docx")}")\
-        .export(projects)
+projects = ProjectConverter()\
+    .convert(json_projects)
 
-export_format_a(load_data())
+FormatAExporter()\
+    .set_file_path(f"{datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S_Projekte.docx")}")\
+    .export(projects)
