278c3dc7ed50d34324c157e8ef70be16a734e5cd
Robert Grüning <r.gruening@web.de>
2025-12-02 23:17:38 +0100

    extracted class

---
+++ formataexporter.py
@@ -0,0 +1,65 @@
+import docx
+from docx.shared import Pt
+
+class FormatAExporter():
+    def __init__(self):
+        self.file_path = None
+
+    def set_file_path(self, file_path:str):
+        self.file_path = file_path
+
+        return self
+
+    def export(self, projects:list):
+        doc = docx.Document()
+        table = doc.add_table(rows = len(projects), cols = 2)
+
+        for i, project in enumerate(projects):
+            row = table.rows[i].cells
+            left_cell = row[0]
+            left_cell.paragraphs.clear()
+            left_cell_paragraph = left_cell.add_paragraph(style=None)
+
+            right_cell = row[1]
+            right_cell.paragraphs.clear()
+
+            timespan = project.StartDate
+
+            if project.EndDate is not None and\
+                project.StartDate != project.EndDate:
+                timespan += f" - {project.EndDate}"
+
+            timespan_run = left_cell_paragraph.add_run(timespan)
+            timespan_run.font.name = 'Arial'
+            timespan_run.font.size = Pt(10)
+            
+            right_cell.add_paragraph(style=None)\
+                .add_run(project.CustomerName)\
+                .bold = True
+
+            role_names = []
+
+            for role in project.Roles:
+                role_names.append(role.Name)
+
+            run_role_names = right_cell.add_paragraph(style=None)\
+                .add_run(', '.join(role_names))
+            run_role_names.font.name = 'Arial'
+            run_role_names.font.size = Pt(10)
+
+            run_role_title = right_cell.add_paragraph(style=None)\
+                .add_run(project.Title)
+            run_role_title.font.name = 'Arial'
+            run_role_title.font.size = Pt(10)
+
+            run_description = right_cell.add_paragraph(style=None)\
+                .add_run(project.Description)
+            run_description.font.name = 'Arial'
+            run_description.font.size = Pt(10)
+            
+            run_tags = right_cell.add_paragraph(style=None)\
+                .add_run(', '.join(project.Tags))
+            run_tags.font.name = 'Arial'
+            run_tags.font.size = Pt(10)
+
+        doc.save(self.file_path)
--- personal-project-log.py
+++ personal-project-log.py
@@ -1,9 +1,8 @@
 import datetime
-import docx
-from docx.shared import Pt
 from json import JSONEncoder
 
 from repository import Repository
+from formataexporter import FormatAExporter
 
 class Role():
     def __init__(self):
@@ -58,58 +57,9 @@ def load_data():
             .load()
     )
 
-def export_format_a(doc, projects):
-    table = doc.add_table(rows = len(projects), cols = 2)
+def export_format_a(projects:list):
+    FormatAExporter()\
+        .set_file_path(f"{datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S_Projekte.docx")}")\
+        .export(projects)
 
-    for i, project in enumerate(projects):
-        row = table.rows[i].cells
-        left_cell = row[0]
-        left_cell.paragraphs.clear()
-        left_cell_paragraph = left_cell.add_paragraph(style=None)
-
-        right_cell = row[1]
-        right_cell.paragraphs.clear()
-
-        timespan = project.StartDate
-
-        if project.EndDate is not None and\
-            project.StartDate != project.EndDate:
-            timespan += f" - {project.EndDate}"
-
-        timespan_run = left_cell_paragraph.add_run(timespan)
-        timespan_run.font.name = 'Arial'
-        timespan_run.font.size = Pt(10)
-        
-        right_cell.add_paragraph(style=None)\
-            .add_run(project.CustomerName)\
-            .bold = True
-
-        role_names = []
-
-        for role in project.Roles:
-            role_names.append(role.Name)
-
-        run_role_names = right_cell.add_paragraph(style=None)\
-            .add_run(', '.join(role_names))
-        run_role_names.font.name = 'Arial'
-        run_role_names.font.size = Pt(10)
-
-        run_role_title = right_cell.add_paragraph(style=None)\
-            .add_run(project.Title)
-        run_role_title.font.name = 'Arial'
-        run_role_title.font.size = Pt(10)
-
-        run_description = right_cell.add_paragraph(style=None)\
-            .add_run(project.Description)
-        run_description.font.name = 'Arial'
-        run_description.font.size = Pt(10)
-        
-        run_tags = right_cell.add_paragraph(style=None)\
-            .add_run(', '.join(project.Tags))
-        run_tags.font.name = 'Arial'
-        run_tags.font.size = Pt(10)
-
-    return doc
-
-export_format_a(docx.Document(), load_data())\
-    .save(f"{datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S_Projekte.docx")}")
+export_format_a(load_data())
