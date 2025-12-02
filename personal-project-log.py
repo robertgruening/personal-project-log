a6710bfd8210c72d0e78dad95bdc4f7068028317
Robert Grüning <r.gruening@web.de>
2025-12-02 23:24:31 +0100

    extracted class; disabled not used code

--- personal-project-log.py
+++ personal-project-log.py
@@ -1,41 +1,17 @@
 import datetime
-from json import JSONEncoder
+#from json import JSONEncoder
 
 from formataexporter import FormatAExporter
-from project import Project
+from projectconverter import ProjectConverter
 from repository import Repository
-from role import Role
 
-class ProjectEncoder(JSONEncoder):
-    def default(self, o):
-        return o.__dict__
+#class ProjectEncoder(JSONEncoder):
+#    def default(self, o):
+#        return o.__dict__
         
 def convert_to_projects(json_projects):
-    projects = []
-
-    for json_project in json_projects:
-        project = Project()
-        project.Title = json_project.get('Title')
-        project.StartDate = json_project.get('StartDate')
-        project.EndDate = json_project.get('EndDate')
-        project.Description = json_project.get('Description')
-        project.CustomerName = json_project.get('CustomerName')
-
-        for json_role in json_project.get('Roles'):
-            role = Role()
-            role.Name = json_role.get('Name')
-
-            for json_task in json_role.get('Tasks'):
-                role.Tasks.append(json_task)
-            
-            project.Roles.append(role)
-
-        for json_tag in json_project.get('Tags'):
-            project.Tags.append(json_tag)
-
-        projects.append(project)
-
-    return projects
+    return ProjectConverter()\
+        .convert(json_projects)
 
 def load_data():
     return convert_to_projects(
---
+++ projectconverter.py
@@ -0,0 +1,34 @@
+from project import Project
+from role import Role
+
+
+class ProjectConverter():
+    def __init__(self):
+        pass
+
+    def convert(json_projects):
+        projects = []
+
+        for json_project in json_projects:
+            project = Project()
+            project.Title = json_project.get('Title')
+            project.StartDate = json_project.get('StartDate')
+            project.EndDate = json_project.get('EndDate')
+            project.Description = json_project.get('Description')
+            project.CustomerName = json_project.get('CustomerName')
+
+            for json_role in json_project.get('Roles'):
+                role = Role()
+                role.Name = json_role.get('Name')
+
+                for json_task in json_role.get('Tasks'):
+                    role.Tasks.append(json_task)
+                
+                project.Roles.append(role)
+
+            for json_tag in json_project.get('Tags'):
+                project.Tags.append(json_tag)
+
+            projects.append(project)
+
+        return projects
