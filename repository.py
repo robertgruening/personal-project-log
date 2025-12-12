b2de275aef36dff72b5e43b776c04eded6084625
Robert Grüning <r.gruening@web.de>
2025-12-12 02:30:15 +0100

    integrated converter in repository

--- repository.py
+++ repository.py
@@ -1,4 +1,5 @@
 import json
+from projectconverter import ProjectConverter
 
 
 class Repository():
@@ -11,7 +12,16 @@ class Repository():
         return self
 
     def load(self):
-        with open(self.file_path, "r") as f:
-            return json.load(f)
+        with open(self.file_path, 'r', encoding='utf-8') as f:
+            json_projects = json.load(f)
+            projects = ProjectConverter().convert(json_projects)
+            
+            return projects
 
         return []
+    
+    def save(self, projects:list):
+        with open(self.file_path, 'w', encoding='utf-8') as f:
+            json_projects = ProjectConverter().convert_to_json(projects)
+            json_string_projects = json.dumps(json_projects, indent=4)
+            f.write(json_string_projects)
