76fcd4016cfde87c152b68459bcc37ad38375bc4
Robert Grüning <r.gruening@web.de>
2025-12-12 02:35:27 +0100

    added apply button; started apply event implementation

--- winforms/projecteditorwindow.py
+++ winforms/projecteditorwindow.py
@@ -3,7 +3,6 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 
-
 class ProjectEditorWindow(tk.Toplevel):
     def __init__(self, parent, project=None):
         super().__init__(parent)
@@ -16,11 +15,11 @@ class ProjectEditorWindow(tk.Toplevel):
         title_label = ttk.Label(self, text="Titel")
         title_label.grid(column=0, row=row_index, sticky=tk.EW, padx=5, pady=5)
 
-        title_entry = ttk.Entry(self)
-        title_entry.grid(column=1, row=row_index, sticky=tk.EW, padx=5, pady=5)
+        self.title_entry = ttk.Entry(self)
+        self.title_entry.grid(column=1, row=row_index, sticky=tk.EW, padx=5, pady=5)
         
         if self.project is not None:
-            title_entry.insert(0, self.project.Title)
+            self.title_entry.insert(0, self.project.Title)
         row_index += 1
         #endregion
 
@@ -29,11 +28,11 @@ class ProjectEditorWindow(tk.Toplevel):
         start_date_label = ttk.Label(self, text="Startdatum")
         start_date_label.grid(column=0, row=row_index, sticky=tk.EW, padx=5, pady=5)
 
-        start_date_entry = ttk.Entry(self)
-        start_date_entry.grid(column=1, row=row_index, sticky=tk.EW, padx=5, pady=5)
+        self.start_date_entry = ttk.Entry(self)
+        self.start_date_entry.grid(column=1, row=row_index, sticky=tk.EW, padx=5, pady=5)
         
         if self.project is not None:
-            start_date_entry.insert(0, self.project.StartDate)
+            self.start_date_entry.insert(0, self.project.StartDate)
         row_index += 1
         #endregion
 
@@ -41,11 +40,11 @@ class ProjectEditorWindow(tk.Toplevel):
         end_date_label = ttk.Label(self, text="Enddatum")
         end_date_label.grid(column=0, row=row_index, sticky=tk.EW, padx=5, pady=5)
 
-        end_date_entry = ttk.Entry(self)
-        end_date_entry.grid(column=1, row=row_index, sticky=tk.EW, padx=5, pady=5)
+        self.end_date_entry = ttk.Entry(self)
+        self.end_date_entry.grid(column=1, row=row_index, sticky=tk.EW, padx=5, pady=5)
         
         if self.project is not None:
-            end_date_entry.insert(0, self.project.EndDate)
+            self.end_date_entry.insert(0, self.project.EndDate)
         row_index += 1
         #endregion
 
@@ -53,11 +52,11 @@ class ProjectEditorWindow(tk.Toplevel):
         customer_name_label = ttk.Label(self, text="Kundenname")
         customer_name_label.grid(column=0, row=row_index, sticky=tk.EW, padx=5, pady=5)
 
-        customer_name_entry = ttk.Entry(self)
-        customer_name_entry.grid(column=1, row=row_index, sticky=tk.EW, padx=5, pady=5)
+        self.customer_name_entry = ttk.Entry(self)
+        self.customer_name_entry.grid(column=1, row=row_index, sticky=tk.EW, padx=5, pady=5)
         
         if self.project is not None:
-            customer_name_entry.insert(0, self.project.CustomerName)
+            self.customer_name_entry.insert(0, self.project.CustomerName)
         row_index += 1
         #endregion
 
@@ -65,11 +64,11 @@ class ProjectEditorWindow(tk.Toplevel):
         customer_location_label = ttk.Label(self, text="Kundenstandort")
         customer_location_label.grid(column=0, row=row_index, sticky=tk.EW, padx=5, pady=5)
 
-        customer_location_entry = ttk.Entry(self)
-        customer_location_entry.grid(column=1, row=row_index, sticky=tk.EW, padx=5, pady=5)
+        self.customer_location_entry = ttk.Entry(self)
+        self.customer_location_entry.grid(column=1, row=row_index, sticky=tk.EW, padx=5, pady=5)
         
         if self.project is not None:
-            customer_location_entry.insert(0, self.project.CustomerLocation)
+            self.customer_location_entry.insert(0, self.project.CustomerLocation)
         row_index += 1
         #endregion
 
@@ -77,17 +76,32 @@ class ProjectEditorWindow(tk.Toplevel):
         description_label = ttk.Label(self, text="Beschreibung")
         description_label.grid(column=0, row=row_index, sticky=tk.EW, padx=5, pady=5)
 
-        description_entry = Text(self, height=10)
-        description_entry.grid(column=1, row=row_index, sticky=tk.EW, padx=5, pady=5)
+        self.description_entry = Text(self, height=10)
+        self.description_entry.grid(column=1, row=row_index, sticky=tk.EW, padx=5, pady=5)
         
         if self.project is not None:
-            description_entry.insert(0, self.project.Description)
+            self.description_entry.insert(END, self.project.Description)
         row_index += 1
         #endregion
 
         #region button save
         frame_buttons = ttk.Frame(self, padding=10)
         ttk.Button(frame_buttons, text="Abbrechen", command=self.destroy).pack(side=tk.LEFT, padx=5)
-        ttk.Button(frame_buttons, text="Speichern").pack(side=tk.LEFT, padx=5)
+        ttk.Button(frame_buttons, text="Übernehmen", command=self.event_button_apply_clicked).pack(side=tk.LEFT, padx=5)
         frame_buttons.grid(column=1, row=row_index, sticky=tk.E, padx=5, pady=5)
         #endregion
+    
+    def event_button_apply_clicked(self):
+        project = Project()
+        project.Title = self.title_entry.get()
+        project.StartDate = self.start_date_entry.get()
+        project.EndDate = self.end_date_entry.get()
+        project.CustomerName = self.customer_name_entry.get()
+        project.CustomerLocation = self.customer_location_entry.get()
+        project.Description = self.description_entry.get("1.0", "end")
+
+        # validate project
+
+        # return to main window
+        
+        self.destroy()
